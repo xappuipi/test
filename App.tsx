d64d00bc4cdbd2750c0186dbea83b08e1c513ab4
xappuipi <134952022+xappuipi@users.noreply.github.com>
2023-10-20 22:10:04 +0200

    new

--- App.tsx
+++ App.tsx
@@ -3,16 +3,22 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import container from './styles/container'
 import opacity from './styles/opacity'
 import text_button from './styles/text_button';
+import { useEffect, useState } from 'react';
+import { lambda } from './cokolwiek';
 
 export default function App() {
+  const [jakis_int, setJakis_int] = useState(0);
+
+
   return (
     <View style={container.container}>
       <View  style={container.container_bottom}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>{jakis_int}</Text>
+        <Text>{lambda()}</Text>
       </View>
       {/* <StatusBar style="auto" /> */}
       <View  style={container.container_bottom}>
-        <TouchableOpacity style={opacity.opacity}>
+        <TouchableOpacity style={opacity.opacity} onPress={()=>{setJakis_int(jakis_int+1)}}>
         <Text style={text_button.color}>abcd</Text>
       </TouchableOpacity>
       </View>
---
+++ cokolwiek.tsx
@@ -0,0 +1,5 @@
+
+export const lambda = () =>{
+    return "ssss";
+}
+
